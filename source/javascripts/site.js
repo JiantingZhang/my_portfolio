1d84eeec78953845052309cefc05e9c4435cebc2
JiantingZhang <zhangjianting481@gmail.com>
2018-05-26 15:42:31 +0800

    float img to left

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,3 +2,17 @@
 // var visitor = prompt("What is your name?")
 // var message = 'Hello ' + visitor + ". Welcome to my website! "
 // document.write(message);
+
+function isEmailEmpty() {
+  var field = document.getElementById('email');
+  if (field.value === '') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var fieldTest = isEmailEmpty();
+if (fieldTest === true) {
+  alert('Please provide your email address.');
+}
